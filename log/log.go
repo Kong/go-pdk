350f70bee91afcb9707a0e40a874989ca714921f
Javier Guerra <javier.guerra@konghq.com>
2019-10-25 03:44:32 -0500

    Remove a level of indirection
    Methods to a struct are aplicable to either the struct or a pointer to it.  Channels are already reference
    types, so no significant gain by adding another indirection.
    
    Methods to pointers, OTOH, apply to a struct only if it is "referencible", and should be used only if the method
    modifies the given value.  (using a channel isn't considered modification)

--- log/log.go
+++ log/log.go
@@ -10,37 +10,38 @@ type Log struct {
 	bridge.PdkBridge
 }
 
-func New(ch chan string) *Log {
-	return &Log{*bridge.New(ch)}
+func New(ch chan string) Log {
+	return Log{bridge.New(ch)}
 }
 
-func (r *Log) Alert(args ...string) {
+func (r Log) Alert(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.alert:%s`, strings.Join(args, "")))
 }
 
-func (r *Log) Crit(args ...string) {
+func (r Log) Crit(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.crit:%s`, strings.Join(args, "")))
 }
 
-func (r *Log) Err(args ...string) {
+func (r Log) Err(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.err:%s`, strings.Join(args, "")))
 }
 
-func (r *Log) Warn(args ...string) {
+func (r Log) Warn(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.warn:%s`, strings.Join(args, "")))
 }
 
-func (r *Log) Notice(args ...string) {
+func (r Log) Notice(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.notice:%s`, strings.Join(args, "")))
 }
 
-func (r *Log) Info(args ...string) {
+func (r Log) Info(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.info:%s`, strings.Join(args, "")))
 }
-func (r *Log) Debug(args ...string) {
+
+func (r Log) Debug(args ...string) {
 	_ = r.Ask(fmt.Sprintf(`kong.log.debug:%s`, strings.Join(args, "")))
 }
 
-func (r *Log) Serialize() string {
+func (r Log) Serialize() string {
 	return r.Ask(fmt.Sprintf(`kong.log.serialize`))
 }
--- response/response.go
+++ response/response.go
@@ -10,21 +10,21 @@ type Response struct {
 	bridge.PdkBridge
 }
 
-func New(ch chan string) *Response {
-	return &Response{*bridge.New(ch)}
+func New(ch chan string) Response {
+	return Response{bridge.New(ch)}
 }
 
-func (r *Response) GetStatus() int {
+func (r Response) GetStatus() int {
 	reply := r.Ask(`kong.response.get_status`)
 	status, _ := strconv.Atoi(reply)
 	return status
 }
 
-func (r *Response) GetHeader(name string) string {
+func (r Response) GetHeader(name string) string {
 	return r.Ask(fmt.Sprintf(`kong.response.get_header:%s`, name))
 }
 
-func (r *Response) GetHeaders(max_headers int) map[string]interface{} {
+func (r Response) GetHeaders(max_headers int) map[string]interface{} {
 	var method string
 	if max_headers == -1 {
 		method = `kong.response.get_headers`
@@ -37,27 +37,27 @@ func (r *Response) GetHeaders(max_headers int) map[string]interface{} {
 	return headers
 }
 
-func (r *Response) GetSource() string {
+func (r Response) GetSource() string {
 	return r.Ask(`kong.response.get_source`)
 }
 
-func (r *Response) SetStatus(status int) {
+func (r Response) SetStatus(status int) {
 	_ = r.Ask(fmt.Sprintf(`kong.response.set_status:%d`, status))
 }
 
-func (r *Response) SetHeader(k string, v string) {
+func (r Response) SetHeader(k string, v string) {
 	_ = r.Ask(fmt.Sprintf(`kong.response.set_header:["%s","%s"]`, k, v))
 }
 
-func (r *Response) AddHeader(k string, v string) {
+func (r Response) AddHeader(k string, v string) {
 	_ = r.Ask(fmt.Sprintf(`kong.response.add_header:["%s","%s"]`, k, v))
 }
 
-func (r *Response) ClearHeader(k string) {
+func (r Response) ClearHeader(k string) {
 	_ = r.Ask(fmt.Sprintf(`kong.response.clear_header:%s`, k))
 }
 
-func (r *Response) SetHeaders(headers map[string]interface{}) error {
+func (r Response) SetHeaders(headers map[string]interface{}) error {
 	headersBytes, err := bridge.Marshal(headers)
 	if err != nil {
 		return err
