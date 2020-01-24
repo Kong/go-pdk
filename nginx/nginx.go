5a18fd130a569b68477df4debf7bfbea99a1159e
Javier <javier.guerra@konghq.com>
2020-01-24 18:20:46 -0500

    feat(*) add missing methods to go-pdk (#22)
    Plus fixes to docs.

--- nginx/nginx.go
+++ nginx/nginx.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Kong/go-pdk/bridge"
 )
 
+// Holds this module's functions.  Accessible as `kong.Nginx`
 type Nginx struct {
 	bridge.PdkBridge
 }
@@ -16,6 +17,7 @@ func New(ch chan interface{}) Nginx {
 	return Nginx{bridge.New(ch)}
 }
 
+// kong.Nginx.GetVar() returns an Nginx variable.  Equivalent to `ngx.var[k]`
 func (n Nginx) GetVar(k string) (string, error) {
 	return n.AskString(`kong.nginx.get_var`, k)
 }
@@ -24,18 +26,29 @@ func (n Nginx) GetTLS1VersionStr() (string, error) {
 	return n.AskString(`kong.nginx.get_tls1_version_str`)
 }
 
+// kong.Nginx.GetCtxAny() returns a value from the `ngx.ctx` request context table.
 func (n Nginx) GetCtxAny(k string) (interface{}, error) {
 	return n.Ask(`kong.nginx.get_ctx`, k)
 }
 
+// kong.Nginx.GetCtxString() returns a string value from the `ngx.ctx` request context table.
 func (n Nginx) GetCtxString(k string) (string, error) {
 	return n.AskString(`kong.nginx.get_ctx`, k)
 }
 
+// kong.Nginx.GetCtxFloat() returns a float value from the `ngx.ctx` request context table.
 func (n Nginx) GetCtxFloat(k string) (float64, error) {
 	return n.AskFloat(`kong.nginx.get_ctx`, k)
 }
 
+// kong.Nginx.ReqStartTime() returns the curent request's start time
+// as a floating-point number of seconds.  Equivalent to `ngx.req.start_time()`
 func (n Nginx) ReqStartTime() (float64, error) {
 	return n.AskFloat(`kong.nginx.req_start_time`)
+}
+
+// kong.Nginx.GetSubsystem() returns the current Nginx subsystem
+// this function is called from: “http” or “stream”.
+func (n Nginx) GetSubsystem() (string, error) {
+	return n.AskString(`kong.nginx.get_subsystem`)
 }
