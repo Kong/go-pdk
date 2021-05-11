74f1bb95fe8ddf626aca7901c81b277d3a470181
Javier Guerra <javier.guerra@konghq.com>
2021-05-11 15:10:53 -0500

    add service req/res / split simulation steps

--- test/test.go
+++ test/test.go
@@ -25,24 +25,28 @@ import (
 	// 	"google.golang.org/protobuf/types/known/structpb"
 )
 
+type headersMap map[string][]string
+
+func (h headersMap) cloneTo(out_h map[string][]string) {
+	for k, v := range h {
+		out_v := make([]string, len(v))
+		for i, sub_v := range v {
+			out_v[i] = sub_v
+		}
+		out_h[k] = out_v
+	}
+}
+func (h headersMap) clone() map[string][]string {
+	out_h := make(map[string][]string, len(h))
+	h.cloneTo(out_h)
+	return out_h
+}
+
 type Request struct {
 	Method  string
 	Url     string
 	Headers map[string][]string
 	Body    string
-}
-
-type Response struct {
-	Status  int
-	Message string
-	Headers map[string][]string
-	Body    string
-}
-
-type clientEnv struct {
-	t   *testing.T
-	req Request
-	res Response
 }
 
 func (req Request) Validate() error {
@@ -55,46 +59,74 @@ func (req Request) Validate() error {
 	return fmt.Errorf("Unsupported method \"%v\"", req.Method)
 }
 
-func (e clientEnv) noErr(err error) {
+func (req Request) clone() Request {
+	return Request{
+		Method:  req.Method,
+		Url:     req.Url,
+		Headers: headersMap(req.Headers).clone(),
+		Body:    req.Body,
+	}
+}
+
+func (req Request) ToResponse(res *Response) {
+	res.Status = 200
+	res.Message = "OK"
+	headersMap(req.Headers).cloneTo(res.Headers)
+	res.Body = req.Body
+}
+
+type Response struct {
+	Status  int
+	Message string
+	Headers map[string][]string
+	Body    string
+}
+
+func (res Response) cloneTo(out *Response) {
+	out.Status = res.Status
+	out.Message = res.Message
+	headersMap(res.Headers).cloneTo(out.Headers)
+	out.Body = res.Body
+}
+
+func (res Response) clone() Response {
+	out := Response{Headers: map[string][]string{}}
+	res.cloneTo(&out)
+	return out
+}
+
+type testEnv struct {
+	t          *testing.T
+	pdk        *pdk.PDK
+	ClientReq  Request
+	ServiceReq Request
+	ServiceRes Response
+	ClientRes  Response
+}
+
+func New(t *testing.T, req Request) (env testEnv, err error) {
+	if req.Headers == nil {
+		req.Headers = map[string][]string{}
+	}
+	err = req.Validate()
 	if err != nil {
-		e.t.Error(err)
+		return
 	}
-}
 
-func (e clientEnv) Errorf(format string, args ...interface{}) {
-	e.t.Errorf(format, args...)
-}
-
-func (e clientEnv) Handle(method string, args_d []byte) []byte {
-	switch method {
-	case "kong.request.get_headers":
-		{
-			out, err := bridge.WrapHeaders(e.req.Headers)
-			e.noErr(err)
-			out_d, err := proto.Marshal(out)
-			e.noErr(err)
-			return out_d
-		}
-
-	case "kong.response.set_header":
-		{
-			out := new(kong_plugin_protocol.KV)
-			e.noErr(proto.Unmarshal(args_d, out))
-			e.res.Headers[out.K] = []string{out.V.GetStringValue()}
-			return nil
-		}
-
-	default:
-		{
-			e.t.Errorf("unknown method: \"%v\"", method)
-			return nil
-		}
+	env = testEnv{
+		t:          t,
+		ClientReq:  req,
+		ServiceReq: req.clone(),
+		ServiceRes: Response{
+			Headers: map[string][]string{},
+		},
+		ClientRes: Response{
+			Headers: map[string][]string{},
+		},
 	}
-}
 
-func mockPdk(e clientEnv) *pdk.PDK {
-	b := bridge.New(bridgetest.MockFunc(e))
-	return &pdk.PDK{
+	b := bridge.New(bridgetest.MockFunc(&env)) // check
+	env.pdk = &pdk.PDK{
 		Client:          client.Client{b},
 		Ctx:             ctx.Ctx{b},
 		Log:             log.Log{b},
@@ -108,41 +140,107 @@ func mockPdk(e clientEnv) *pdk.PDK {
 		ServiceRequest:  service_request.Request{b},
 		ServiceResponse: service_response.Response{b},
 	}
+	return
 }
 
-func (req Request) Exec(t *testing.T, config interface{}) (res Response, err error) {
-	err = req.Validate()
+func (e testEnv) noErr(err error) {
 	if err != nil {
-		return
+		e.t.Error(err)
 	}
+}
 
-	res = Response{
-		Status:  200,
-		Headers: make(map[string][]string),
+func (e testEnv) Errorf(format string, args ...interface{}) {
+	e.t.Errorf(format, args...)
+}
+
+func (e *testEnv) Handle(method string, args_d []byte) []byte {
+	switch method {
+	case "kong.request.get_headers":
+		{
+			out, err := bridge.WrapHeaders(e.ClientReq.Headers)
+			e.noErr(err)
+			out_d, err := proto.Marshal(out)
+			e.noErr(err)
+			return out_d
+		}
+
+	case "kong.response.set_header":
+		{
+			out := new(kong_plugin_protocol.KV)
+			e.noErr(proto.Unmarshal(args_d, out))
+			e.ClientRes.Headers[out.K] = []string{out.V.GetStringValue()}
+			return nil
+		}
+
+	default:
+		{
+			e.t.Errorf("unknown method: \"%v\"", method)
+			return nil
+		}
 	}
+}
 
-	mPdk := mockPdk(clientEnv{t, req, res})
-
+func (e *testEnv) DoCertificate(config interface{}) {
 	if h, ok := config.(interface{ Certificate(*pdk.PDK) }); ok {
-		t.Log("Certificate")
-		h.Certificate(mPdk)
+		e.t.Log("Certificate")
+		h.Certificate(e.pdk)
 	}
-	if h, ok := config.(interface{ Rewrite(*pdk.PDK) }); ok {
-		t.Log("Rewrite")
-		h.Rewrite(mPdk)
-	}
-	if h, ok := config.(interface{ Access(*pdk.PDK) }); ok {
-		t.Log("Access")
-		h.Access(mPdk)
-	}
-	if h, ok := config.(interface{ Response(*pdk.PDK) }); ok {
-		t.Log("Response")
-		h.Response(mPdk)
-	}
-	if h, ok := config.(interface{ Log(*pdk.PDK) }); ok {
-		t.Log("Log")
-		h.Log(mPdk)
-	}
+}
 
-	return
+func (e *testEnv) DoRewrite(config interface{}) {
+	if h, ok := config.(interface{ Rewrite(*pdk.PDK) }); ok {
+		e.t.Log("Rewrite")
+		h.Rewrite(e.pdk)
+	}
+}
+
+func (e *testEnv) DoAccess(config interface{}) {
+	if h, ok := config.(interface{ Access(*pdk.PDK) }); ok {
+		e.t.Log("Access")
+		h.Access(e.pdk)
+	}
+}
+
+func (e *testEnv) DoResponse(config interface{}) {
+	e.ServiceRes.cloneTo(&e.ClientRes)
+	if h, ok := config.(interface{ Response(*pdk.PDK) }); ok {
+		e.t.Log("Response")
+		h.Response(e.pdk)
+	}
+}
+
+func (e *testEnv) DoPreread(config interface{}) {
+	if h, ok := config.(interface{ Preread(*pdk.PDK) }); ok {
+		e.t.Log("Preread")
+		h.Preread(e.pdk)
+	}
+}
+
+func (e *testEnv) DoLog(config interface{}) {
+	if h, ok := config.(interface{ Log(*pdk.PDK) }); ok {
+		e.t.Log("Log")
+		h.Log(e.pdk)
+	}
+}
+
+func (e *testEnv) DoHttp(config interface{}) {
+	e.DoAccess(config)
+	e.ServiceReq.ToResponse(&e.ServiceRes)		// assuming an "echo service"
+	e.DoResponse(config)
+	e.DoLog(config)
+}
+
+func (e *testEnv) DoHttps(config interface{}) {
+	e.DoCertificate(config)
+	e.DoHttp(config)
+}
+
+func (e *testEnv) DoStream(config interface{}) {
+	e.DoPreread(config)
+	e.DoLog(config)
+}
+
+func (e *testEnv) DoTLS(config interface{}) {
+	e.DoCertificate(config)
+	e.DoStream(config)
 }
