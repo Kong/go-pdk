d01cea27620f53fec17df78956b8b99b1f92df45
Matthieu MOREL <mmorel-35@users.noreply.github.com>
2021-06-30 14:20:25 +0200

    Update install-protoc.sh

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-PROTOC_VERSION=3.14.0
+PROTOC_VERSION=3.15.8
 PROTOC_ZIP=protoc-$PROTOC_VERSION-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
