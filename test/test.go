ec6dca04b40cd78484771f4fb904a28a10c9d922
Winslow DiBona <dibona.winslow@gmail.com>
2024-03-06 19:30:44 -0500

    tests(*): fix ctx test handlers (#187)
    The usage of `bridge.WrapString` diverges the testing tool from how production interaction with the context is performed.
    This resulted in working context handling when running the library but resulted in issues when running tests.
    
    Using structpb.NewValue aligns the testing utility with how the library interacts with kong in production through its usage of `structpb.Value`
    
    Tests were added to verify setting/getting in the shared context works for the testing utility.

--- test/test.go
+++ test/test.go
@@ -288,13 +288,14 @@ func (e *TestEnv) Handle(method string, args_d []byte) []byte {
 	case "kong.ctx.shared.set":
 		args := kong_plugin_protocol.KV{}
 		e.noErr(proto.Unmarshal(args_d, &args))
-		e.Ctx.Store[args.K] = args.V
+		e.Ctx.Store[args.K] = args.V.AsInterface()
 
 	case "kong.ctx.shared.get":
 		args := kong_plugin_protocol.String{}
 		e.noErr(proto.Unmarshal(args_d, &args))
-		v := fmt.Sprintf("%v", e.Ctx.Store[args.V])
-		out = bridge.WrapString(v)
+		if e.Ctx.Store[args.V] != nil {
+			out, err = structpb.NewValue(e.Ctx.Store[args.V])
+		}
 
 	case "kong.ip.is_trusted":
 		out = &kong_plugin_protocol.Bool{V: true}
--- test/test_test.go
+++ test/test_test.go
@@ -6,7 +6,12 @@ import (
 	"testing"
 
 	"github.com/Kong/go-pdk"
+	"github.com/Kong/go-pdk/bridge"
+	"github.com/Kong/go-pdk/server/kong_plugin_protocol"
 	"github.com/stretchr/testify/assert"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type foo struct {
@@ -36,4 +41,52 @@ func TestNoHangingChannel(t *testing.T) {
 	})
 	assert.NoError(t, err)
 	env.DoHttps(MockNew())
+}
+
+func TestSharedContext(t *testing.T) {
+	key := "TestKey"
+
+	t.Parallel()
+
+	env, err := New(t, Request{
+		Method: "POST",
+		Url:    "",
+		Body:   []byte("{}"),
+	})
+	assert.NoError(t, err)
+
+	perform := func(v interface{}) {
+		// Test set
+		value, err := structpb.NewValue(v)
+		assert.NoError(t, err)
+		setArgs, err := proto.Marshal(&kong_plugin_protocol.KV{K: key, V: value})
+		assert.NoError(t, err)
+
+		env.Handle("kong.ctx.shared.set", setArgs)
+		// Assert kv pair is in Ctx.Store
+		assert.Equal(t, v, env.Ctx.Store[key])
+
+		// Test get
+		keyWrapped := bridge.WrapString(key)
+		getArgs, _ := proto.Marshal(keyWrapped)
+		response := env.Handle("kong.ctx.shared.get", getArgs)
+
+		out := new(structpb.Value)
+		err = proto.Unmarshal(response, out)
+		assert.NoError(t, err)
+		assert.Equal(t, v, out.AsInterface())
+	}
+
+	testValues := []interface{}{
+		"TestValue",
+		3.14,
+		false,
+		map[string]interface{}{
+			"key1": "value1",
+		},
+	}
+
+	for _, v := range testValues {
+		perform(v)
+	}
 }
